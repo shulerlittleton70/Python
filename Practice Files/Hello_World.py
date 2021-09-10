0b5d4acb014f97c70425414c987195356a3be398
Shuler Littleton <shulerlittleton70@gmail.com>
2021-09-10 15:25:15 -0400

    Hello World Task

---
+++ Practice Files/Hello_World.py
@@ -0,0 +1,6 @@
+print('Hello, World!') #Standard String Print
+print(1+2) #Prints Result
+print(7*6) #Prints Result
+print() #Prints Blank
+print("The End") #Standard String
+print("The End","or is it?","Keep watching to learn more about Python", 3) #Ties each piece together as one string
