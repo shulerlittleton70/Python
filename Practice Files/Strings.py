64a104ac8aa128075a7d6fd9b714357f389b87c5
Shuler Littleton <shulerlittleton70@gmail.com>
2021-09-10 15:42:49 -0400

    Input Function

---
+++ Practice Files/Strings.py
@@ -0,0 +1,14 @@
+print("Today is a good day to learn Python")
+print('Python is fun') #Python can use' or ", but you have to start and finish with the same
+print("Python's Strings are easy to use")
+print('We can even include "quotes" in Strings') #Can use ' and " in a string but have to use opposite to contain
+print("Hello"+" World") #String Concatination
+
+
+#variables
+
+greetings = "Hello"
+name = input("Please Enter Your Name") #Input function request a value from user and saves as variable
+
+#combine variables, add space
+print(greetings + " " + name)
