41d667bf8aa6cc8aa37bcb053f7a13ace31892bd
Shuler Littleton <shulerlittleton70@gmail.com>
2021-09-10 16:18:05 -0400

    type function

--- Practice Files/Variables_and_types.py
+++ Practice Files/Variables_and_types.py
@@ -1,8 +1,11 @@
-#When a prgram is running, everything the program needs ends up stored in the computers memory.
-#The program code itself will be stored in one area of memory, and the data it works on will also be
+#When a prgram is running, everything the program
+# needs ends up stored in the computers memory.
+#The program code itself will be stored in one area of
+# memory, and the data it works on will also be
 #Stored somewhere in memory
 
-#A variable is basically just a way to give a meaningful name to an area of memory, into which we can place
+#A variable is basically just a way to give a meaningful
+# name to an area of memory, into which we can place
 #certain values
 
 greeting = "Hello"
