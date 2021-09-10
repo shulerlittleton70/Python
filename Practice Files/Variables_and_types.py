3b1bcad28d6534d73d898d68a0c647b2a12ecb18
Shuler Littleton <shulerlittleton70@gmail.com>
2021-09-10 16:12:58 -0400

    Variables

---
+++ Practice Files/Variables_and_types.py
@@ -0,0 +1,7 @@
+#When a prgram is running, everything the program needs ends up stored in the computers memory.
+#The program code itself will be stored in one area of memory, and the data it works on will also be
+#Stored somewhere in memory
+
+#A variable is basically just a way to give a meaningful name to an area of memory, into which we can place
+#certain values
+
