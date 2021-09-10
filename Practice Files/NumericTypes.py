f31470e60a2c834374f4b5c9ae49cd892e18ddd6
Shuler Littleton <shulerlittleton70@gmail.com>
2021-09-10 16:35:30 -0400

    Numeric Types and Operations

---
+++ Practice Files/NumericTypes.py
@@ -0,0 +1,14 @@
+#Python 3 has three numeric types:
+#int
+#Float
+#complex
+
+a = 12
+b = 3
+
+print(a+b) #addition
+print(a-b) #subtraction
+print(a*b) #multiply
+print(a / b) #Devide
+print(a//b) #integerDivision
+print(a%b) #modulo: Remainder after interger division
