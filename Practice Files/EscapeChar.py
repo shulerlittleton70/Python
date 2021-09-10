2ce02c1668c59d66a11a020618c6623ca77b13d4
Shuler Littleton <shulerlittleton70@gmail.com>
2021-09-10 15:55:58 -0400

    \n = next line in string \t = tab in string

---
+++ Practice Files/EscapeChar.py
@@ -0,0 +1,9 @@
+SplitSting = "This String has been\nsplit over\nseveral\nlines" #\n opperator sends string to next line
+print(SplitSting)
+
+tabbedString = "1\t2\t3\t4\t5" #\t inserts tab
+print(tabbedString)
+
+print('The pet shop owner said "No,no, \'e\'s uh, ...he\'s resting".')
+
+print("""The pet shop owner said "No, no, 'e's uh, .... he's resting".""") #three consecutive quotes will wait on
