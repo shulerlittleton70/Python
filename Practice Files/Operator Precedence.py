7c5843ee98d0ba3ee8558946d2f8666fc067676c
Shuler Littleton <shulerlittleton70@gmail.com>
2021-09-13 19:05:27 -0400

    Order of opps

--- Practice Files/Operator Precedence.py
+++ Practice Files/Operator Precedence.py
@@ -1,1 +1,26 @@
+a = 12
+b = 3
+
+print(a+b/3-4*12)
+print(a+(b/3)-(4*12))
+print((((a+b)/3)-4)*12)
+
+#Please excuse my dear aunt sally.
+
+#PEMDAS - Parentheses, Exponents, Multi/Div, Add/Sub
+#BEDMAS - Brackets, Exponents, Div/Multi, Add/Sub
+#BODMAS - Brackets, order, Div/Multi,Add/Sub
+#BIDMAS - Brackets, Index, Division/Multi, Add/Sub
+
+#The above can at times be confusing:
+# Multi/Div have equal precedence, but are read lefft to write
+
+c = a + b
+d = c / 3
+e = d - 4
+print(e * 12)
+
+print()
+
+print(a / (b * a) /b)
 
