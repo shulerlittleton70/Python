f06ef13105257c640b1cf271de5510b9673db73b
Shuler Littleton <shulerlittleton70@gmail.com>
2021-09-17 15:20:00 -0400

    Intro

---
+++ 002 Program Flow/001 Blocks and Statements.py
@@ -0,0 +1,10 @@
+#Like many languages, Python works in blocks of code.
+
+#One of the design principles of Python, thoughm was that it
+#should be uncluttered and easy to read.
+
+#Because of this, Python doesnt use delimiters around blocks of code.
+
+#If you have used other languages, you may have seen them use braces {} or BEGIN and END around code.
+#Python doesnt use anything like thatm but instead uses indentation to indicate when a new block starts.
+
