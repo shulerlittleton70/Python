e03dc4cd517c471aad7aa2a7b06d5625a831a1ca
Shuler Littleton <shulerlittleton70@gmail.com>
2021-09-17 14:19:40 -0400

    Negative String

--- Practice Files/Slicing With Negative Numbers.py
+++ Practice Files/Slicing With Negative Numbers.py
@@ -8,4 +8,9 @@ print(parrot[0:6]) #Norweg
 print(parrot[-4:-2]) #Bl
 print(parrot[-4:12]) #Bl
 
+#Can also do steps
+
+print(parrot[o:6:2]) #Nre  The code is read in 2 steps
+
+
 
---
+++ Practice Files/String Operators (Or any Sequence types).py
@@ -0,0 +1,41 @@
+#Python 3 Contains 5 Sequence types built in
+#1 - String Type
+#2 - List Type
+#3 - Tuple Type
+#4 - Range Type
+#5 - Bytes and Bytearray Types
+
+#What is a sequence?
+# A sequence is defined as an ordered set of items
+##1 - String Type
+print("Hello World")
+
+#2 - List Type
+x = ["Computer","Monitor","Mouse"]
+print(x)
+
+print(x[1])
+
+######################################################################
+
+string1 = "He's "
+string2 = "probably "
+string3 = "pining "
+string4 = "for the "
+string5 = "fjords."
+
+print(string1+string2+string3+string4+string5)
+print("He's " "probably " "pining " "for the " "fjords.")
+
+print("Hello " * 5)
+
+
+today = "friday"
+
+print("day" in today) #True
+print("fri" in today) #True
+print("thur" in today) #False
+print("parrot" in today) #False
+
+
+
