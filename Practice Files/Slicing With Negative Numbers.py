3fc289e2e0730a05cb2346da092d901ee2bfa86e
Shuler Littleton <shulerlittleton70@gmail.com>
2021-09-17 14:03:49 -0400

    Negative String

---
+++ Practice Files/Slicing With Negative Numbers.py
@@ -0,0 +1,11 @@
+#Slicing with negative numbers.
+
+parrot = "Norwegian Blue"
+
+print(parrot[6:0]) #Error, can not go in reverse without negative indexing
+print(parrot[0:6]) #Norweg
+
+print(parrot[-4:-2]) #Bl
+print(parrot[-4:12]) #Bl
+
+
