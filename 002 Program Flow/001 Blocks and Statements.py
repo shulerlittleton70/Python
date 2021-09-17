52831dbd03c98c35bf3286b3503d442e0d2d4806
Shuler Littleton <shulerlittleton70@gmail.com>
2021-09-17 15:31:55 -0400

    Intro

--- 002 Program Flow/001 Blocks and Statements.py
+++ 002 Program Flow/001 Blocks and Statements.py
@@ -8,3 +8,16 @@
 #If you have used other languages, you may have seen them use braces {} or BEGIN and END around code.
 #Python doesnt use anything like thatm but instead uses indentation to indicate when a new block starts.
 
+#Whenever a line ends with a :, this tells Python that a new code block is starting
+#Backspace pulls out of block
+    #Shift/Tab will also exit
+#Tab can take you back to it
+
+#############First Try at Blocks#####################
+
+for i in range(1, 13):
+    print("No. {} squared is {} and cubed is {:4}".format(i, i ** 2, i ** 3))
+    print("*" * 80) #creates a line of *
+
+
+
