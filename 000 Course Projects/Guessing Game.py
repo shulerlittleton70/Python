1f118d126a9b8127d7e629616bb2d3ceb1dee646
Shuler Littleton <shulerlittleton70@gmail.com>
2021-09-17 16:46:24 -0400

    Finsihed Project

---
+++ 000 Course Projects/Guessing Game.py
@@ -0,0 +1,29 @@
+import time as t
+
+answer = 5
+
+print("Please guess an number between 1 and 10: ")
+guess = int(input())
+
+if guess < answer:
+    print("Please guess higher: ")
+    guess = int(input())
+    if guess == answer:
+        print("Well done! You guessed it!")
+    else:
+        print("GAME OVER")
+        t.sleep(5)
+
+
+elif guess > answer:
+    print ("Please guess lower: ")
+    guess = int(input())
+    if guess == answer:
+        print("Well done! You guessed it!")
+    else:
+        print("GAME OER")
+        t.sleep(5)
+
+else:
+    print("You got it the first time!")
+    t.sleep(5)
