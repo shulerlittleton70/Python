8e6d6cba10153393a279f729b0ea9c6c95c88fb4
Shuler Littleton <shulerlittleton70@gmail.com>
2021-09-17 15:44:14 -0400

    Voting age request program.

---
+++ 002 Program Flow/002 IF STATEMENTS.py
@@ -0,0 +1,25 @@
+#Input request input on the behalf of the user
+#The below code:
+    #Request Name (ENTER)
+    #Request Age from Name inserted (Enter)
+    #Prints the Age given
+    #Int () will keep the input/output as a number (This limits age input to a int or Error)
+
+
+name = input("Please enter your name: ")
+age = int(input("How old are you, {0}?".format(name)))
+print(age)
+
+#IF statement for program to check age for programming
+
+if age >= 18:
+    print("You are old enough to vote!")
+else:
+    print("You will have to come back in {0} years! Thank you".format(18-age))
+
+
+
+
+
+
+
