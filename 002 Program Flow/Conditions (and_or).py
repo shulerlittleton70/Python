5b478cd751e8e5fd86b932214172d2f0a15d6c9c
Shuler Littleton <shulerlittleton70@gmail.com>
2021-10-20 14:47:16 -0400

    Finsihed Project

--- 002 Program Flow/Conditions (and_or).py
+++ 002 Program Flow/Conditions (and_or).py
@@ -1,6 +1,14 @@
 age = int(input("How old are you?"))
 
-if age >= 16 and age <= 65:
+#if age >= 16 and age <= 65:
+if 16<=age<= 65: #Chained Comparison
     print("Have a great day at work!")
 else:
-    print("Go home!")
+    print("Enjoy your free time!")
+
+print("-" * 80) #visual line in execution block
+
+if age < 16 or age >65:
+    print("Enjoy your free time")
+else:
+    print ("Have a great day at work!")
