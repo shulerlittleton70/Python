90f91fff167b6fbe22ae0ab4edc3bc8d0178e684
Shuler Littleton <shulerlittleton70@gmail.com>
2021-10-20 14:52:21 -0400

    Finsihed Project

--- 002 Program Flow/006 TrueFalse.py
+++ 002 Program Flow/006 TrueFalse.py
@@ -2,7 +2,7 @@ day = "Monday"
 Temp = 30
 Rain = True
 
-if day == "Saturday" and Temp > 27 and not raining:
+if day == "Saturday" and Temp > 27 and not Rain:
     print("Go Swimming!")
 else:
     print("Learn Python!")
