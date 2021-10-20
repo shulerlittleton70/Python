464bf46f702e83651773d9dc3f97a9b165ea4a64
Shuler Littleton <shulerlittleton70@gmail.com>
2021-10-20 14:37:09 -0400

    Finsihed Project

--- 002 Program Flow/guessinggame.py
+++ 002 Program Flow/guessinggame.py
@@ -5,25 +5,55 @@ answer = 5
 print("Please guess an number between 1 and 10: ")
 guess = int(input())
 
-if guess < answer:
-    print("Please guess higher: ")
+if guess != answer:
+    if guess < answer:
+        print("Please Guess Higher")
+    else: # guess must be greater than
+        print("Ple3"
+              "ase Guess Lower")
     guess = int(input())
     if guess == answer:
-        print("Well done! You guessed it!")
+        print("Well done, you guessed it!")
     else:
-        print("GAME OVER")
-        t.sleep(5)
-
-
-elif guess > answer:
-    print ("Please guess lower: ")
-    guess = int(input())
-    if guess == answer:
-        print("Well done! You guessed it!")
-    else:
-        print("GAME OER")
-        t.sleep(5)
-
+        print("Sorry, you have not guessed correctly")
 else:
     print("You got it the first time!")
-    t.sleep(5)
+
+# if guess < answer:
+#     print("Please guess higher: ")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done! You guessed it!")
+#     else:
+#         print("GAME OVER")
+#         t.sleep(5)
+#
+#
+# elif guess > answer:
+#     print ("Please guess lower: ")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done! You guessed it!")
+#     else:
+#         print("GAME OER")
+#         t.sleep(5)
+#
+# else:
+#     print("You got it the first time!")
+#     t.sleep(5)
+
+
+
+#less than <
+#Less than or equal to <=
+#Greater than >
+#Greater than or equal to >=
+#Equal to ==
+#Not Equal to !=
+
+
+
+
+
+
+
