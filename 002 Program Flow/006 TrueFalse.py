948cf460c0232e0850a55a847d6cb641a46734c0
Shuler Littleton <shulerlittleton70@gmail.com>
2021-10-20 14:57:20 -0400

    Finsihed Project

--- 002 Program Flow/006 TrueFalse.py
+++ 002 Program Flow/006 TrueFalse.py
@@ -2,7 +2,7 @@ day = "Saturday"
 Temp = 30
 Rain = True
 
-if (day == "Saturday" and Temp > 27) or not Rain:
+if (day == "Saturday" and Temp > 27) or not Rain: #use () when using and and or in the same expresions
     print("Go Swimming!")
 else:
     print("Learn Python!")
